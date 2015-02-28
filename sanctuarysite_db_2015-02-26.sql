9c6eb97cd881e60401ba504c47e66e67f91ca77e
Jenna Murphy <jmurphy@mcad.edu>
2015-02-28 17:21:10 -0600

    adjusted site url

--- sanctuarysite_db_2015-02-26.sql
+++ sanctuarysite_db_2015-02-26.sql
@@ -121,8 +121,8 @@ LOCK TABLES `wp_options` WRITE;
 
 INSERT INTO `wp_options` (`option_id`, `option_name`, `option_value`, `autoload`)
 VALUES
-	(1,'siteurl','http://sanctuary.jennadeemurphy.com','yes'),
-	(2,'home','http://sanctuary.jennadeemurphy.com','yes'),
+	(1,'siteurl','http://www.www.sanctuary.jennadeemurphy.com','yes'),
+	(2,'home','http://www.sanctuary.jennadeemurphy.com','yes'),
 	(3,'blogname','Sanctuary','yes'),
 	(4,'blogdescription','Just another WordPress site','yes'),
 	(5,'users_can_register','0','yes'),
@@ -332,9 +332,9 @@ LOCK TABLES `wp_posts` WRITE;
 
 INSERT INTO `wp_posts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`)
 VALUES
-	(1,1,'2015-02-27 04:09:57','2015-02-27 04:09:57','Welcome to WordPress. This is your first post. Edit or delete it, then start blogging!','Hello world!','','publish','open','open','','hello-world','','','2015-02-27 04:09:57','2015-02-27 04:09:57','',0,'http://sanctuary.jennadeemurphy.com/?p=1',0,'post','',1),
-	(2,1,'2015-02-27 04:09:57','2015-02-27 04:09:57','This is an example page. It\'s different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this:\n\n<blockquote>Hi there! I\'m a bike messenger by day, aspiring actor by night, and this is my blog. I live in Los Angeles, have a great dog named Jack, and I like pi&#241;a coladas. (And gettin\' caught in the rain.)</blockquote>\n\n...or something like this:\n\n<blockquote>The XYZ Doohickey Company was founded in 1971, and has been providing quality doohickeys to the public ever since. Located in Gotham City, XYZ employs over 2,000 people and does all kinds of awesome things for the Gotham community.</blockquote>\n\nAs a new WordPress user, you should go to <a href=\"http://sanctuary.jennadeemurphy.com/wp-admin/\">your dashboard</a> to delete this page and create new pages for your content. Have fun!','Sample Page','','publish','open','open','','sample-page','','','2015-02-27 04:09:57','2015-02-27 04:09:57','',0,'http://sanctuary.jennadeemurphy.com/?page_id=2',0,'page','',0),
-	(3,1,'2015-02-27 04:10:17','0000-00-00 00:00:00','','Auto Draft','','auto-draft','open','open','','','','','2015-02-27 04:10:17','0000-00-00 00:00:00','',0,'http://sanctuary.jennadeemurphy.com/?p=3',0,'post','',0);
+	(1,1,'2015-02-27 04:09:57','2015-02-27 04:09:57','Welcome to WordPress. This is your first post. Edit or delete it, then start blogging!','Hello world!','','publish','open','open','','hello-world','','','2015-02-27 04:09:57','2015-02-27 04:09:57','',0,'http://www.sanctuary.jennadeemurphy.com/?p=1',0,'post','',1),
+	(2,1,'2015-02-27 04:09:57','2015-02-27 04:09:57','This is an example page. It\'s different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this:\n\n<blockquote>Hi there! I\'m a bike messenger by day, aspiring actor by night, and this is my blog. I live in Los Angeles, have a great dog named Jack, and I like pi&#241;a coladas. (And gettin\' caught in the rain.)</blockquote>\n\n...or something like this:\n\n<blockquote>The XYZ Doohickey Company was founded in 1971, and has been providing quality doohickeys to the public ever since. Located in Gotham City, XYZ employs over 2,000 people and does all kinds of awesome things for the Gotham community.</blockquote>\n\nAs a new WordPress user, you should go to <a href=\"http://www.sanctuary.jennadeemurphy.com/wp-admin/\">your dashboard</a> to delete this page and create new pages for your content. Have fun!','Sample Page','','publish','open','open','','sample-page','','','2015-02-27 04:09:57','2015-02-27 04:09:57','',0,'http://www.sanctuary.jennadeemurphy.com/?page_id=2',0,'page','',0),
+	(3,1,'2015-02-27 04:10:17','0000-00-00 00:00:00','','Auto Draft','','auto-draft','open','open','','','','','2015-02-27 04:10:17','0000-00-00 00:00:00','',0,'http://www.sanctuary.jennadeemurphy.com/?p=3',0,'post','',0);
 
 /*!40000 ALTER TABLE `wp_posts` ENABLE KEYS */;
 UNLOCK TABLES;
